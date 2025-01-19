b3f97d288c240a20212b6fabe4eff798b19c62a4
wellingtonfcdev <wellingtonfc@hotmail.com>
2025-01-19 00:41:41 -0300

    Criação da classe MedicoController para envido dos dados

---
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -0,0 +1,9 @@
+package med.vol.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+}
