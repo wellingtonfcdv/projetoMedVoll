8132e5030a3388e2eee05d74e3acbde84b41ae87
wellingtonfcdev <wellingtonfc@hotmail.com>
2025-01-19 01:34:26 -0300

    Criação de classes record para receber os dados via json

--- src/main/java/med/vol/api/controller/MedicoController.java
+++ src/main/java/med/vol/api/controller/MedicoController.java
@@ -1,9 +1,18 @@
 package med.vol.api.controller;
 
+import med.vol.api.medico.DadosCadastroMedico;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/medicos")
+@RequestMapping("medicos")
 public class MedicoController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+        System.out.println(dados);
+
+    }
 }
